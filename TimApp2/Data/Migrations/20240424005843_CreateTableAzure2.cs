02b69f879d6a8ebb77df6f48a35c0c2e4b3f9d0e
XudongHeAllen <hex3456@gmail.com>
2024-04-23 21:03:47 -0400

    change database to Azure

---
+++ TimApp2/Data/Migrations/20240424005843_CreateTableAzure2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TimApp2.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTableAzure2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
